7822f08e9c0b07e23a67969ad2e72d97996aa949
Ryan Brubaker <ryan.brubaker@gmail.com>
2024-11-17 11:57:49 -0600

    Consolidated code fragments in push class

--- project8/pop_command.py
+++ project8/pop_command.py
@@ -1,5 +1,5 @@
 POP_VIRTUAL_FRAGMENT = """@{}
-D=M
+D={}
 @{}
 D=D+A
 @R13
@@ -12,22 +12,7 @@ A=M
 M=D
 """
 
-POP_TEMP_FRAGMENT = """@{}
-D=A
-@{}
-D=D+A
-@R13
-M=D
-@SP
-AM=M-1
-D=M
-@R13
-A=M
-M=D
-"""
-
-
-POP_POINTER_FRAGMENT = """@SP
+POP_DIRECT_FRAGMENT = """@SP
 AM=M-1
 D=M
 @{}
@@ -38,13 +23,10 @@ VIRTUAL_KEY_MAP = {
     "local":    "LCL",
     "argument": "ARG",
     "this":     "THIS",
-    "that":     "THAT"  
+    "that":     "THAT",
+    "temp":     "R5"
 }
 
-STATIC_LOC_MAP = {
-    "temp":     5,
-    "static":   16
-}
 
 POINTER_LOC_MAP = {
     "0":    "THIS",
@@ -56,14 +38,13 @@ class PopCommand:
     
     def __init__(self, segment, index, filename):
         self._code = ""
-        if segment == "local" or segment == "argument" or segment == "this" or segment == "that":
-            self._code = POP_VIRTUAL_FRAGMENT.format(VIRTUAL_KEY_MAP[segment], index)
-        elif segment == "temp":
-            self._code = POP_TEMP_FRAGMENT.format("R5", index)
+        if segment == "local" or segment == "argument" or segment == "this" or segment == "that" or segment == "temp":
+            A_or_M = "A" if segment == "temp" else "M"
+            self._code = POP_VIRTUAL_FRAGMENT.format(VIRTUAL_KEY_MAP[segment], A_or_M, index)
         elif segment == "static":
-            self._code = POP_POINTER_FRAGMENT.format(filename + "." + index)
+            self._code = POP_DIRECT_FRAGMENT.format(filename + "." + index)
         elif segment == "pointer":
-            self._code = POP_POINTER_FRAGMENT.format(POINTER_LOC_MAP[index])
+            self._code = POP_DIRECT_FRAGMENT.format(POINTER_LOC_MAP[index])
 
         
     @property
